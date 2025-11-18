aed5c6cc8ac164602131791ee7a65059adba57ee
Yangmoooo <yangmoooo@outlook.com>
2025-11-19 01:16:44 +0800

    feat(config): add sources config type

--- src/package.rs
+++ src/package.rs
@@ -1,12 +1,58 @@
-mod remote;
+mod remotes;
+
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Default, Deserialize)]
+pub struct PackageConfig {
+    #[serde(flatten)]
+    pub pkgs: HashMap<String, PackageInfo>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PackageInfo {
+    #[serde(default)]
+    pub ver: Option<String>,
+    pub remote: Remote,
+    #[serde(default)]
+    pub bin: Vec<BinConfig>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Remote {
+    GitHub(GitHubSource),
+    // Direct(DirectSource),
+}
+
+impl Default for Remote {
+    fn default() -> Self {
+        Remote::GitHub(GitHubSource::default())
+    }
+}
+
+#[derive(Debug, Default, Deserialize)]
+pub struct GitHubSource {
+    pub repo: String,
+    #[serde(default)]
+    pub asset: HashMap<String, String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct BinConfig {
+    #[serde(default)]
+    pub path: Option<String>,
+    #[serde(default)]
+    pub link: Option<String>,
+}
 
 #[derive(Debug)]
 pub struct PackgeReceipt {
     pub name: String,
-    pub version: String,
+    pub ver: String,
     pub bins: Vec<String>,
 }
-
 
 #[derive(thiserror::Error, Debug)]
 pub enum PackageError {
@@ -17,7 +63,7 @@ pub enum PackageError {
     Remote {
         name: String,
         #[source]
-        source: remote::Error,
+        source: remotes::Error,
     },
 
     #[error("Download failed for '{url}'")]
@@ -42,5 +88,4 @@ pub enum PackageError {
 
     #[error("Filesystem IO error: {0}")]
     Io(#[from] std::io::Error),
-
 }
