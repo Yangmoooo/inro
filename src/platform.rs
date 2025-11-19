2e55e2436c3eb807e7b4de6e6ef4c3404bafd30a
Yangmoooo <yangmoooo@outlook.com>
2025-11-19 19:56:02 +0800

    feat(platform): add partial match

--- src/platform.rs
+++ src/platform.rs
@@ -1,3 +1,4 @@
+use core::slice;
 use std::env::consts;
 
 #[derive(Debug)]
@@ -11,6 +12,27 @@ impl PlatformInfo {
         Self {
             os: consts::OS,
             arch: consts::ARCH,
+        }
+    }
+    pub fn key(&self) -> String {
+        format!("{}-{}", self.os, self.arch)
+    }
+
+    pub fn os_aliases(&self) -> &[&str] {
+        match self.os {
+            "linux" => &["linux"],
+            "macos" => &["darwin", "apple", "macos", "osx"],
+            "windows" => &["windows", "-win", "_win"],
+            _ => slice::from_ref(&self.os),
+        }
+    }
+
+    pub fn arch_aliases(&self) -> &[&str] {
+        match self.arch {
+            "x86_64" => &["x86_64", "amd64", "x64"],
+            "aarch64" => &["aarch64", "arm64"],
+            "x86" => &["x86", "i386", "i686", "386"],
+            _ => slice::from_ref(&self.arch),
         }
     }
 }
