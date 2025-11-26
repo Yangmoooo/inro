171cda468f825cc00214a3acd8d76bb0aea0f04f
Yangmoooo <yangmoooo@outlook.com>
2025-11-27 00:06:16 +0800

    feat(registry): extract sources config from the original package mod and implement its load

---
+++ src/registry.rs
@@ -0,0 +1,65 @@
+use std::collections::HashMap;
+use std::fs::read_dir;
+
+use anyhow::{Result, anyhow};
+use figment::Figment;
+use figment::providers::{Format, Toml};
+use serde::Deserialize;
+
+use crate::dan::*;
+use crate::layout::InroLayout;
+
+#[derive(Debug, Default, Deserialize)]
+pub struct Registry {
+    #[serde(flatten)]
+    pub dans: HashMap<String, DanDef>,
+}
+
+impl Registry {
+    pub fn load(layout: &InroLayout) -> Result<Self> {
+        let mut figment = Figment::new();
+
+        // load upstream registry
+        let upstream_registry_dir = &layout.upstream_registry_dir;
+        if upstream_registry_dir.exists() {
+            let mut files = vec![];
+            for entry in read_dir(upstream_registry_dir)? {
+                let entry = entry?;
+                let path = entry.path();
+                if path.extension().and_then(|s| s.to_str()) == Some("toml") {
+                    files.push(path);
+                }
+            }
+
+            files.sort();
+            for file_path in files {
+                figment = figment.merge(Toml::file(file_path));
+            }
+        }
+
+        // load local registry
+        let local_registry_dir = &layout.local_registry_dir;
+        if local_registry_dir.exists() {
+            let mut files = vec![];
+            for entry in read_dir(local_registry_dir)? {
+                let entry = entry?;
+                let path = entry.path();
+                if path.extension().and_then(|s| s.to_str()) == Some("toml") {
+                    files.push(path);
+                }
+            }
+
+            files.sort();
+            for file_path in files {
+                figment = figment.merge(Toml::file(file_path));
+            }
+        }
+
+        let registry: Registry = figment.extract()?;
+        if registry.dans.is_empty() {
+            return Err(anyhow!("No package definitions found in sources"));
+        }
+
+        Ok(registry)
+    }
+}
